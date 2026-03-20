16afdbce8e0065bfe5612ccd6ea6b2cd6705bc05
Kelebra <ricardokelebra@gmail.com>
2026-03-20 03:12:42 -0600

    delete guards

--- src/lib/auth/guards.ts
+++
@@ -1,8 +0,0 @@
-import { redirect } from 'next/navigation';
-import { getCurrentSession } from './session';
-
-export async function requireSession() {
-  const session = await getCurrentSession();
-  if (!session) redirect('/login');
-  return session;
-}
